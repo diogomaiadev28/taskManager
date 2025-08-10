ccc119d5ce2fe7f77db9444e4c5ebc8ade26146a
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-08-10 12:59:27 -0300

    feat: Adição da codificação HTML da página principal.

---
+++ View/mainPage.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Main Page | Task Manager</title>
+        <link rel="shortcut icon" href="../templates/assets/img/logo.ico" type="image/x-icon">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <figure class="logo"><img src="../templates/assets/img/logo.png" alt="" class="logo"></figure>
+                <div class="rightNav">
+                    <button>+</button>
+                    <div class="profile">
+                        <div class="names">
+                            <h2>Gabriela Salomão</h2>
+                            <h4>gabrielasalomao.sesiba@gmail.com</h4>
+                        </div>
+                        <figure class="pImg"><img src="../templates/assets/img/profile.png" alt="" class="pImg"></figure>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="today">
+                    <h2>Deadline today 🔥</h2>
+                    <div class="cont">
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="tomorrow">
+                    <h2>Tomorrow or beyond 🚀</h2>
+                    <div class="cont">
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton">Do</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="done">
+                    <h2>Done ✅</h2>
+                    <div class="cont">
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton1">Done</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton1">Done</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton1">Done</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="late">
+                    <h2>Late ❌</h2>
+                    <div class="cont">
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton2">Expired</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton2">Expired</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="cardData">
+                                <div class="title">
+                                    <h3>Finish the school's slide at hello</h3>
+                                    <figure class="pencil"><img class="pencil" src="../templates/assets/img/pencil.png" alt=""></figure>
+                                </div>
+                                <h5>Slide about kids health at schools and something just to fullfill the</h5>
+                                <div class="button">
+                                    <h4>08/13/2025</h4>
+                                    <button class="cardButton2">Expired</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
