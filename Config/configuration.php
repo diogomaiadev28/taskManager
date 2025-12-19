3063ba1e453157b9d5c878017abc6547fada0377
DiogoMaiaSESI <diogomaia.sesiba@gmail.com>
2025-12-18 21:48:20 -0300

    feat: Criação do arquivo Dockerfile.

--- Config/configuration.php
+++ Config/configuration.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_NAME','task_manager');
-define('DB_HOST','localhost');
+define('DB_HOST','maglev.proxy.rlwy.net');
 define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_PORT','3306');
+define('DB_PASSWORD','FeQfiIcgJnglqGHwFzqhpMfSSaWSuJcG');
+define('DB_PORT','20696');
 ?>
